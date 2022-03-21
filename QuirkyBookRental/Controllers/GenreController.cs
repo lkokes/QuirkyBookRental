bdb76c4a57086fb71a556bb900fd4383afda41c9
Lucia Kokesova <lkokesova@gmail.com>
2022-03-21 16:45:38 +0100

    Initial commit

---
+++ QuirkyBookRental/Controllers/GenreController.cs
@@ -0,0 +1,29 @@
+﻿using QuirkyBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuirkyBookRental.Controllers
+{
+    public class GenreController : Controller
+    {
+        private ApplicationDbContext db;
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
